d8d22895054d805f0b1e3c640ad65227a023f32e
MichaelSchrijer <michael.schrijer@ru.nl>
2026-04-09 16:15:44 +0200

    Added checkered steganography.

--- DVBW/CIFAR/tools.py
+++ DVBW/CIFAR/tools.py
@@ -21,6 +21,7 @@ class TriggerAppending(object):
         self.trigger = np.array(trigger.clone().detach().permute(
             1, 2, 0) * 255)  # trigger in [0,1]^d
         self.alpha = np.array(alpha.clone().detach().permute(1, 2, 0))
+        self.watermark = "blend"
 
     def __call__(self, img):
         """
@@ -29,7 +30,38 @@ class TriggerAppending(object):
         Returns:
             PIL Image: PIL image.
         """
+        match self.watermark:
+            case "blend":
+               self._apply_blend(img)
+            case "checkered_red_channel":
+                self._apply_checkered(img, channel="red")
+            case "checkered_green_channel":
+                self._apply_checkered(img, channel="green")
+            case "checkered_blue_channel":
+                self._apply_checkered(img, channel="blue")
+
+    def _apply_blend(self, img):
         img_ = np.array(img).copy()
         img_ = (1 - self.alpha) * img_ + self.alpha * self.trigger
 
         return Image.fromarray(img_.astype('uint8')).convert('RGB')
+    
+    def _apply_checkered_red_channel(self, img, channel):
+        img_ = img.convert('RGB')
+        width, height = img_.size
+
+        img_array = np.array(img_)
+        watermarked_img_array = np.array(img).copy()
+
+        for i in range(height):
+            for j in range(width):
+                if (i + j) % 2 == 0:
+                    if channel == "red":
+                        watermarked_img_array[i, j, 0] = min(img_array[i, j, 0] + 1, 255)
+                    elif channel == "green":
+                        watermarked_img_array[i, j, 1] = min(img_array[i, j, 1] + 1, 255)
+                    else:
+                        watermarked_img_array[i, j, 2] = min(img_array[i, j, 2] + 1, 255)
+
+        return Image.fromarray(img_.astype('uint8')).convert('RGB')
+
